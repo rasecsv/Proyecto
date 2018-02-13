df724e5b4a23ea7b672023550f52f12ac4a0ef8c
zigotero <jashirchirre@gmail.com>
2018-02-13 12:54:14 -0500

    Cambios en personal y registro
    atributos y metods cambiados

--- ProyectoMate/src/clases/Personal.java
+++ ProyectoMate/src/clases/Personal.java
@@ -10,5 +10,52 @@ package clases;
  * @author Gamer
  */
 class Personal {
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String cargo;
+    private String Apellido;
+
+    public Personal(String id, String nombre, String apellido, String Apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.Apellido = Apellido;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+  
+    
     
 }
--- ProyectoMate/src/clases/Registro.java
+++ ProyectoMate/src/clases/Registro.java
@@ -3,8 +3,33 @@ package clases;
 import java.util.Date;
 
 public class Registro {
-    int ID;
-    Date Hora;
-    Date Dia;
+
+    private String id;
+    private int horaregistroentrada;
+    private int horaregistrosalida;
+    private boolean estado;
+
+    public Registro() {
+    }
+
+    public Registro(String id, int horaregistroentrada, int horaregistrosalida, boolean estado) {
+        this.id = id;
+        this.horaregistroentrada = horaregistroentrada;
+        this.horaregistrosalida = horaregistrosalida;
+        this.estado = estado;
+    }
+    
+    
+    void tardaza(){
+        if(horaregistroentrada>=8){
+            estado=false;
+            System.out.println("Usted ha llegado tarde");
+        }else{
+            estado=true;
+            System.out.println("Usted ha llegado temprano");
+        }
+    }
+
+    
     
 }
